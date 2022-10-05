77f63c6b283c404f0f4ec4cc8052b39bdc0bd66f
Nguyễn Đăng Nam <ndnam4922@gmail.com>
2022-10-05 22:11:46 +0700

    first commit

---
+++ QLSV/BTVN01.sql
@@ -0,0 +1,77 @@
+﻿-- BÀI TẬP TRUY VẤN
+-- 4.1 Cho biết danh sách sinh viên khoa 'Công nghệ thông tin' khóa 2002-2006
+SELECT SV.*
+FROM Lop L, SinhVien SV, KhoaHoc KH
+WHERE L.maKhoa = 'CNTT' AND KH.namBatDau = '2002' AND KH.namKetThuc = '2006'
+AND SV.maLop = L.ma AND KH.ma = L.maKhoaHoc
+-- 4.2 Cho biết các sinh viên(MSSV, Họ tên, Năm sinh) của các sinh viên học sớm hơn tuổi quy định
+--(theo tuổi quy định thì sinh viên đủ 18 tuổi khi bắt đầu khóa học)
+SELECT SV.ma, SV.hoTen, SV.namSinh
+FROM SinhVien SV, KhoaHoc KH, Lop L
+WHERE (KH.namBatDau - SV.namSinh < 18) 
+AND SV.maLop = L.ma AND KH.ma = L.maKhoaHoc
+--4.3 Cho biết sinh viên khoa CNTT, khóa 2002-2006 chưa học môn cấu trúc dữ liệu 1
+SELECT SV.*
+FROM SinhVien SV, KhoaHoc KH, Lop L, KetQua KQ
+WHERE L.maKhoa = 'CNTT' AND KH.namBatDau = '2002' AND KH.namKetThuc = '2006'
+AND SV.maLop = L.ma AND KH.ma = L.maKhoaHoc AND SV.ma = KQ.maSinhVien 
+AND SV.ma NOT IN (SELECT KQ1.maSinhVien FROM KetQua KQ1 WHERE KQ1.maMonHoc = N'THCS01') 
+-- 4.4 Cho biết sinh viên thi không đậu môn Cấu trúc dữ liệu 1 nhưng chưa thi lại
+SELECT SV.*
+FROM SinhVien SV, KetQua KQ
+WHERE SV.ma = KQ.maSinhVien
+AND KQ.maMonHoc = N'THCS01' AND KQ.diem < 5 AND KQ.lanThi < 2
+-- 4.5 Với mỗi lớp thuộc khoa CNTT, cho biết mã lớp, mã khóa học, tên chương trình và số sinh viên thuộc lớp đó. 
+SELECT L.ma, L.maKhoaHoc, CT.tenChuongTrinh, count(sv.maLop) AS SoLuongSinhVien
+FROM Lop L, ChuongTrinh CT, SinhVien SV
+WHERE L.maChuongTrinh = CT.ma AND SV.maLop = L.ma
+AND L.maKhoa = 'CNTT'
+GROUP BY L.ma, L.maKhoaHoc, CT.tenChuongTrinh
+-- 4.6 Cho biết điểm trung bình của sinh viên có mã số 0212003 (điểm trung bình chỉ tính trên lần thi sau cùng của sinh viên)
+SELECT AVG(KQ.diem) AS DiemTrungBinh
+FROM KETQUA KQ
+WHERE KQ.maSinhVien = '0212003'
+
+-- BÀI TẬP FUNCTION	
+-- 5.1 Với 1 mã sinh viên và 1 mã khoa, kiểm tra xem sinh viên có thuộc khoa này hay không (trả về đúng hoặc sai)
+GO
+ALTER FUNCTION F_KiemTra (@maSV varchar(10), @maKhoa varchar(10))
+RETURNS BIT
+AS
+BEGIN 
+	DECLARE @hopLe int
+	SET @hopLe = 1
+	-- Kiểm tra xem mã sinh viên có tồn tại hay không
+	IF NOT EXISTS (SELECT * FROM SinhVien WHERE @maSV = ma)
+		RETURN 0
+	-- Kiểm tra xem mã khoa có tồn tại hay không
+	IF NOT EXISTS (SELECT * FROM Khoa WHERE @maKhoa = ma)
+		RETURN 0
+	-- Kiểm tra sinh viên có thuộc khoa này không 
+	
+	RETURN 1
+END
+GO
+
+-- 5.2 Tính điểm thi sau cùng của một sinh viên trong một môn học cụ thể
+GO
+CREATE FUNCTION F_TinhDiemTB (@maSV varchar(10), @maMH varchar(10))
+RETURNS FLOAT
+AS
+BEGIN
+	RETURN 1
+END
+GO
+-- 5.3 Tính điểm trung bình của sinh viên (điểm trung bình dựa vào lần thi sau cùng), sử dụng function đã viết ở 5.2
+-- 5.4 Nhập vào một sinh viên và một môn học, trả về điểm thi của sinh viên này trong các lần thi của môn học đó
+-- 5.5 Nhập vào một sinh viên, trả về danh sách các môn học mà sinh viên này phải học 
+
+-- BÀI TẬP STORED PROCEDURE
+-- 6.1 In danh sách các sinh viên của một lớp học
+-- 6.2 Nhập vào 2 sinh viên, 1 môn học. Tìm xem sinh viên nào có điểm thi môn học lần đầu tiên là cao hơn
+-- 6.3 Nhập vào 1 môn học và 1 mã sinh viên. Kiểm tra sinh viên có đậu môn này trong lần đầu tiên không. Nếu đậu thì xuất ra "Đậu", nếu không thì xuất ra "Không đậu"
+-- 6.4 Nhập vào một khoa, in danh sách các sinh viên(mã sinh viên, họ tên, ngày sinh) thuộc khoa này
+-- 6.5 Nhập vào một sinh viên và một môn học, in điểm thi của sinh viên này của các lần thi
+-- 6.6 Nhập vào một sinh viên, in ra các môn học mà sinh viên này phải học
+-- 6.7 Nhập vào một môn học, in danh sách các sinh viên đậu môn này trong lần thi đầu tiên
+-- 6.8 In điểm các môn học của sinh viên có mã số là maSinhVien được nhập vào (điểm môn học là điểm của lần thi sau cùng)
---
+++ QLSV/QLSinhVien.sql
@@ -0,0 +1,351 @@
+﻿USE master
+GO
+IF DB_ID('QLSV') IS NOT NULL
+	DROP DATABASE QLSV
+GO
+	CREATE DATABASE QLSV
+GO
+	USE QLSV
+GO
+IF  EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[dbo].[FK_GiangKhoa_ChuongTrinh]') AND type = 'F')
+ALTER TABLE [dbo].[GiangKhoa] DROP CONSTRAINT [FK_GiangKhoa_ChuongTrinh]
+GO
+IF  EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[dbo].[FK_GiangKhoa_Khoa]') AND type = 'F')
+ALTER TABLE [dbo].[GiangKhoa] DROP CONSTRAINT [FK_GiangKhoa_Khoa]
+GO
+IF  EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[dbo].[FK_GiangKhoa_MonHoc]') AND type = 'F')
+ALTER TABLE [dbo].[GiangKhoa] DROP CONSTRAINT [FK_GiangKhoa_MonHoc]
+GO
+IF  EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[dbo].[FK_KetQua_MonHoc]') AND type = 'F')
+ALTER TABLE [dbo].[KetQua] DROP CONSTRAINT [FK_KetQua_MonHoc]
+GO
+IF  EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[dbo].[FK_KetQua_SinhVien]') AND type = 'F')
+ALTER TABLE [dbo].[KetQua] DROP CONSTRAINT [FK_KetQua_SinhVien]
+GO
+IF  EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[dbo].[FK_Lop_ChuongTrinh]') AND type = 'F')
+ALTER TABLE [dbo].[Lop] DROP CONSTRAINT [FK_Lop_ChuongTrinh]
+GO
+IF  EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[dbo].[FK_Lop_Khoa]') AND type = 'F')
+ALTER TABLE [dbo].[Lop] DROP CONSTRAINT [FK_Lop_Khoa]
+GO
+IF  EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[dbo].[FK_Lop_KhoaHoc]') AND type = 'F')
+ALTER TABLE [dbo].[Lop] DROP CONSTRAINT [FK_Lop_KhoaHoc]
+GO
+IF  EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[dbo].[FK_MonHoc_Khoa]') AND type = 'F')
+ALTER TABLE [dbo].[MonHoc] DROP CONSTRAINT [FK_MonHoc_Khoa]
+GO
+IF  EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[dbo].[FK_SinhVien_Lop]') AND type = 'F')
+ALTER TABLE [dbo].[SinhVien] DROP CONSTRAINT [FK_SinhVien_Lop]
+GO
+IF  EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[dbo].[GiangKhoa]') AND OBJECTPROPERTY(id, N'IsUserTable') = 1)
+DROP TABLE [dbo].[GiangKhoa]
+GO
+IF  EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[dbo].[KetQua]') AND OBJECTPROPERTY(id, N'IsUserTable') = 1)
+DROP TABLE [dbo].[KetQua]
+GO
+IF  EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[dbo].[Lop]') AND OBJECTPROPERTY(id, N'IsUserTable') = 1)
+DROP TABLE [dbo].[Lop]
+GO
+IF  EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[dbo].[MonHoc]') AND OBJECTPROPERTY(id, N'IsUserTable') = 1)
+DROP TABLE [dbo].[MonHoc]
+GO
+IF  EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[dbo].[ChuongTrinh]') AND OBJECTPROPERTY(id, N'IsUserTable') = 1)
+DROP TABLE [dbo].[ChuongTrinh]
+GO
+IF  EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[dbo].[Khoa]') AND OBJECTPROPERTY(id, N'IsUserTable') = 1)
+DROP TABLE [dbo].[Khoa]
+GO
+IF  EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[dbo].[KhoaHoc]') AND OBJECTPROPERTY(id, N'IsUserTable') = 1)
+DROP TABLE [dbo].[KhoaHoc]
+GO
+IF  EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[dbo].[SinhVien]') AND OBJECTPROPERTY(id, N'IsUserTable') = 1)
+DROP TABLE [dbo].[SinhVien]
+
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+IF NOT EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[dbo].[ChuongTrinh]') AND OBJECTPROPERTY(id, N'IsUserTable') = 1)
+BEGIN
+CREATE TABLE [dbo].[ChuongTrinh](
+	[ma] [varchar](10) NOT NULL,
+	[tenChuongTrinh] [nvarchar](100) NULL,
+ CONSTRAINT [PK_ChuongTrinh] PRIMARY KEY CLUSTERED 
+(
+	[ma] ASC
+) ON [PRIMARY]
+) ON [PRIMARY]
+END
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+IF NOT EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[dbo].[KhoaHoc]') AND OBJECTPROPERTY(id, N'IsUserTable') = 1)
+BEGIN
+CREATE TABLE [dbo].[KhoaHoc](
+	[ma] [varchar](10) NOT NULL,
+	[namBatDau] [int] NULL,
+	[namKetThuc] [int] NULL,
+ CONSTRAINT [PK_KhoaHoc] PRIMARY KEY CLUSTERED 
+(
+	[ma] ASC
+) ON [PRIMARY]
+) ON [PRIMARY]
+END
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+IF NOT EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[dbo].[Khoa]') AND OBJECTPROPERTY(id, N'IsUserTable') = 1)
+BEGIN
+CREATE TABLE [dbo].[Khoa](
+	[ma] [varchar](10) NOT NULL,
+	[tenKhoa] [nvarchar](100) NULL,
+	[namThanhLap] [int] NULL,
+ CONSTRAINT [PK_Khoa] PRIMARY KEY CLUSTERED 
+(
+	[ma] ASC
+) ON [PRIMARY]
+) ON [PRIMARY]
+END
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+IF NOT EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[dbo].[KetQua]') AND OBJECTPROPERTY(id, N'IsUserTable') = 1)
+BEGIN
+CREATE TABLE [dbo].[KetQua](
+	[maSinhVien] [varchar](10) NOT NULL,
+	[maMonHoc] [varchar](10) NOT NULL,
+	[lanThi] [int] NOT NULL,
+	[diem] [float] NULL,
+ CONSTRAINT [PK_KetQua] PRIMARY KEY CLUSTERED 
+(
+	[maSinhVien] ASC,
+	[maMonHoc] ASC,
+	[lanThi] ASC
+) ON [PRIMARY]
+) ON [PRIMARY]
+END
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+IF NOT EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[dbo].[SinhVien]') AND OBJECTPROPERTY(id, N'IsUserTable') = 1)
+BEGIN
+CREATE TABLE [dbo].[SinhVien](
+	[ma] [varchar](10) NOT NULL,
+	[hoTen] [nvarchar](100) NULL,
+	[namSinh] [int] NULL,
+	[danToc] [nvarchar](20) NULL,
+	[maLop] [varchar](10) NULL,
+ CONSTRAINT [PK_SinhVien] PRIMARY KEY CLUSTERED 
+(
+	[ma] ASC
+) ON [PRIMARY]
+) ON [PRIMARY]
+END
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+IF NOT EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[dbo].[GiangKhoa]') AND OBJECTPROPERTY(id, N'IsUserTable') = 1)
+BEGIN
+CREATE TABLE [dbo].[GiangKhoa](
+	[maChuongTrinh] [varchar](10) NOT NULL,
+	[maKhoa] [varchar](10) NOT NULL,
+	[maMonHoc] [varchar](10) NOT NULL,
+	[namHoc] [int] NULL,
+	[hocKy] [int] NULL,
+	[soTietLyThuyet] [int] NULL,
+	[soTietThucHanh] [int] NULL,
+	[soTinChi] [int] NULL,
+ CONSTRAINT [PK_GiangKhoa] PRIMARY KEY CLUSTERED 
+(
+	[maChuongTrinh] ASC,
+	[maKhoa] ASC,
+	[maMonHoc] ASC
+) ON [PRIMARY]
+) ON [PRIMARY]
+END
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+IF NOT EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[dbo].[Lop]') AND OBJECTPROPERTY(id, N'IsUserTable') = 1)
+BEGIN
+CREATE TABLE [dbo].[Lop](
+	[ma] [varchar](10) NOT NULL,
+	[maKhoaHoc] [varchar](10) NULL,
+	[maKhoa] [varchar](10) NULL,
+	[maChuongTrinh] [varchar](10) NULL,
+	[soThuTu] [int] NULL,
+ CONSTRAINT [PK_Lop] PRIMARY KEY CLUSTERED 
+(
+	[ma] ASC
+) ON [PRIMARY]
+) ON [PRIMARY]
+END
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+IF NOT EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[dbo].[MonHoc]') AND OBJECTPROPERTY(id, N'IsUserTable') = 1)
+BEGIN
+CREATE TABLE [dbo].[MonHoc](
+	[ma] [varchar](10) NOT NULL,
+	[tenMonHoc] [nvarchar](100) NULL,
+	[maKhoa] [varchar](10) NULL,
+ CONSTRAINT [PK_MonHoc] PRIMARY KEY CLUSTERED 
+(
+	[ma] ASC
+) ON [PRIMARY]
+) ON [PRIMARY]
+END
+GO
+IF NOT EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[dbo].[FK_KetQua_MonHoc]') AND type = 'F')
+ALTER TABLE [dbo].[KetQua]  WITH CHECK ADD  CONSTRAINT [FK_KetQua_MonHoc] FOREIGN KEY([maMonHoc])
+REFERENCES [dbo].[MonHoc] ([ma])
+GO
+ALTER TABLE [dbo].[KetQua] CHECK CONSTRAINT [FK_KetQua_MonHoc]
+GO
+IF NOT EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[dbo].[FK_KetQua_SinhVien]') AND type = 'F')
+ALTER TABLE [dbo].[KetQua]  WITH CHECK ADD  CONSTRAINT [FK_KetQua_SinhVien] FOREIGN KEY([maSinhVien])
+REFERENCES [dbo].[SinhVien] ([ma])
+GO
+ALTER TABLE [dbo].[KetQua] CHECK CONSTRAINT [FK_KetQua_SinhVien]
+GO
+IF NOT EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[dbo].[FK_SinhVien_Lop]') AND type = 'F')
+ALTER TABLE [dbo].[SinhVien]  WITH CHECK ADD  CONSTRAINT [FK_SinhVien_Lop] FOREIGN KEY([maLop])
+REFERENCES [dbo].[Lop] ([ma])
+GO
+ALTER TABLE [dbo].[SinhVien] CHECK CONSTRAINT [FK_SinhVien_Lop]
+GO
+IF NOT EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[dbo].[FK_GiangKhoa_ChuongTrinh]') AND type = 'F')
+ALTER TABLE [dbo].[GiangKhoa]  WITH CHECK ADD  CONSTRAINT [FK_GiangKhoa_ChuongTrinh] FOREIGN KEY([maChuongTrinh])
+REFERENCES [dbo].[ChuongTrinh] ([ma])
+GO
+ALTER TABLE [dbo].[GiangKhoa] CHECK CONSTRAINT [FK_GiangKhoa_ChuongTrinh]
+GO
+IF NOT EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[dbo].[FK_GiangKhoa_Khoa]') AND type = 'F')
+ALTER TABLE [dbo].[GiangKhoa]  WITH CHECK ADD  CONSTRAINT [FK_GiangKhoa_Khoa] FOREIGN KEY([maKhoa])
+REFERENCES [dbo].[Khoa] ([ma])
+GO
+ALTER TABLE [dbo].[GiangKhoa] CHECK CONSTRAINT [FK_GiangKhoa_Khoa]
+GO
+IF NOT EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[dbo].[FK_GiangKhoa_MonHoc]') AND type = 'F')
+ALTER TABLE [dbo].[GiangKhoa]  WITH CHECK ADD  CONSTRAINT [FK_GiangKhoa_MonHoc] FOREIGN KEY([maMonHoc])
+REFERENCES [dbo].[MonHoc] ([ma])
+GO
+ALTER TABLE [dbo].[GiangKhoa] CHECK CONSTRAINT [FK_GiangKhoa_MonHoc]
+GO
+IF NOT EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[dbo].[FK_Lop_ChuongTrinh]') AND type = 'F')
+ALTER TABLE [dbo].[Lop]  WITH CHECK ADD  CONSTRAINT [FK_Lop_ChuongTrinh] FOREIGN KEY([maChuongTrinh])
+REFERENCES [dbo].[ChuongTrinh] ([ma])
+GO
+ALTER TABLE [dbo].[Lop] CHECK CONSTRAINT [FK_Lop_ChuongTrinh]
+GO
+IF NOT EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[dbo].[FK_Lop_Khoa]') AND type = 'F')
+ALTER TABLE [dbo].[Lop]  WITH CHECK ADD  CONSTRAINT [FK_Lop_Khoa] FOREIGN KEY([maKhoa])
+REFERENCES [dbo].[Khoa] ([ma])
+GO
+ALTER TABLE [dbo].[Lop] CHECK CONSTRAINT [FK_Lop_Khoa]
+GO
+IF NOT EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[dbo].[FK_Lop_KhoaHoc]') AND type = 'F')
+ALTER TABLE [dbo].[Lop]  WITH CHECK ADD  CONSTRAINT [FK_Lop_KhoaHoc] FOREIGN KEY([maKhoaHoc])
+REFERENCES [dbo].[KhoaHoc] ([ma])
+GO
+ALTER TABLE [dbo].[Lop] CHECK CONSTRAINT [FK_Lop_KhoaHoc]
+GO
+IF NOT EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[dbo].[FK_MonHoc_Khoa]') AND type = 'F')
+ALTER TABLE [dbo].[MonHoc]  WITH CHECK ADD  CONSTRAINT [FK_MonHoc_Khoa] FOREIGN KEY([maKhoa])
+REFERENCES [dbo].[Khoa] ([ma])
+GO
+ALTER TABLE [dbo].[MonHoc] CHECK CONSTRAINT [FK_MonHoc_Khoa]
+
+
+insert into Khoa
+values('CNTT', N'Công nghệ thông tin', 1995)
+insert into Khoa
+values('VL', N'Vật lý', 1970)
+
+insert into KhoaHoc
+values('K2002', 2002, 2006)
+insert into KhoaHoc
+values('K2003', 2003, 2007)
+insert into KhoaHoc
+values('K2004', 2004, 2008)
+
+insert into ChuongTrinh
+values('CQ', N'Chính qui')
+
+insert into Lop
+values('TH2002/01', 'K2002', 'CNTT', 'CQ', 1)
+insert into Lop
+values('TH2002/02', 'K2002', 'CNTT', 'CQ', 2)
+insert into Lop
+values('VL2003/01', 'K2003', 'VL', 'CQ', 1)
+
+insert into SinhVien
+values('0212001', N'Nguyễn Vĩnh An', 1984, N'Kinh', 'TH2002/01')
+insert into SinhVien
+values('0212002', N'Nguyễn Thanh Bình', 1985, N'Kinh', 'TH2002/01')
+insert into SinhVien
+values('0212003', N'Nguyễn Thanh Cường', 1984, N'Kinh', 'TH2002/02')
+insert into SinhVien
+values('0212004', N'Nguyễn Quốc Duy', 1983, N'Kinh', 'TH2002/02')
+insert into SinhVien
+values('0312001', N'Phan Tuấn Anh', 1985, N'Kinh', 'VL2003/01')
+insert into SinhVien
+values('0312002', N'Huỳnh Thanh Sang', 1984, N'Kinh', 'VL2003/01')
+
+insert into MonHoc
+values('THT01', N'Toán Cao cấp A1', 'CNTT')
+insert into MonHoc
+values('VLT01', N'Vật lý Cao cấp A1', 'VL')
+insert into MonHoc
+values('THT02', N'Toán rời rạc', 'CNTT')
+insert into MonHoc
+values('THCS01', N'Cấu trúc dữ liệu 1', 'CNTT')
+insert into MonHoc
+values('THCS02', N'Hệ điều hành', 'CNTT')
+
+insert into KetQua
+values('0212001', 'THT01', 1, 4)
+insert into KetQua
+values('0212001', 'THT01', 2, 7)
+insert into KetQua
+values('0212002', 'THT01', 1, 8)
+insert into KetQua
+values('0212003', 'THT01', 1, 6)
+insert into KetQua
+values('0212004', 'THT01', 1, 9)
+insert into KetQua
+values('0212001', 'THT02', 1, 8)
+insert into KetQua
+values('0212002', 'THT02', 1, 5.5)
+insert into KetQua
+values('0212003', 'THT02', 1, 4)
+insert into KetQua
+values('0212003', 'THCS01', 2, 6)
+insert into KetQua
+values('0212001', 'THCS01', 1, 6.5)
+insert into KetQua
+values('0212002', 'THCS01', 1, 4)
+insert into KetQua
+values('0212003', 'THCS01', 1, 7)
+
+insert into GiangKhoa
+values('CQ', 'CNTT', 'THT01', 2003, 1, 60, 0, 5)
+insert into GiangKhoa
+values('CQ', 'CNTT', 'THT02', 2003, 2, 45, 0, 4)
+insert into GiangKhoa
+values('CQ', 'CNTT', 'THCS01', 2004, 1, 45, 30, 4)
